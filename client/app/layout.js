c626fc36795f1ae10e1da30424e8d28dfc319645
Alexandra Shbaro <alexandrashbaro@gmail.com>
2025-01-15 15:41:36 +0200

    added missing html tags in layout.js

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,17 +1,20 @@
 import "./globals.css";
-import React from 'react';
+import React from "react";
 import Header from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
 export default function RootLayout({ children }) {
-    return (
-      <div className="min-h-screen bg-[#FAFAFA]">
-        <Sidebar />
-        <div className="pl-64">
-          <Header />
-          <main className="p-6">{children}</main>
+  return (
+    <html lang="en">
+      <body className="min-h-screen bg-[#FAFAFA]">
+        <div className="flex">
+          <Sidebar />
+          <div className="flex-grow pl-64">
+            <Header />
+            <main className="p-6">{children}</main>
+          </div>
         </div>
-      </div>
-    );
+      </body>
+    </html>
+  );
 }
-  
