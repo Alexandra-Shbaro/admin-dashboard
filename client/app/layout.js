c3102a07e37d13c132f2573ed9148912e766012a
Alexandra Shbaro <alexandrashbaro@gmail.com>
2025-01-15 13:50:40 +0200

    added layout to include sidebar and header

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,16 +1,17 @@
 import "./globals.css";
-
-export const metadata = {
-  title: "Lumi",
-  description: "",
-};
+import React from 'react';
+import Header from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+      <div className="min-h-screen bg-[#FAFAFA]">
+        <Sidebar />
+        <div className="pl-64">
+          <Header />
+          <main className="p-6">{children}</main>
+        </div>
+      </div>
+    );
 }
+  
